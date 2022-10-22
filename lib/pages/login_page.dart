abac039daa3c033ab6591952f1b4604fba4efdd2
Daniel070999 <danielixo99@gmail.com>
2022-10-22 16:47:07 -0500

    trabajo en front end de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttersupabase/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -73,47 +74,133 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(40),
-        child: ListView(
-          children: [
-            const Padding(padding: EdgeInsets.symmetric(vertical: 100)),
-            const Center(
-                child: Text(
-                    'Ingrese su correo para obtener un enalce de registro',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    textAlign: TextAlign.center)),
-            const SizedBox(height: 40),
-            TextFormField(
-              cursorColor: Colors.white,
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: "Correo electrónico",
-                labelStyle: const TextStyle(color: Colors.white),
-                fillColor: Colors.white,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: const BorderSide(
-                    color: Colors.white,
-                    width: 2.0,
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.lightBlue,
+                Colors.lightBlue,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 36.0, horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "Texto",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w200,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white, width: 2.0),
-                  borderRadius: BorderRadius.circular(25.0),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          style: const TextStyle(color: Colors.lightBlue),
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: "Correo electrónico",
+                            labelStyle:
+                                const TextStyle(color: Colors.lightBlue),
+                            prefixIcon: const Icon(
+                              Icons.email_outlined,
+                              color: Colors.lightBlue,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: const BorderSide(
+                                color: Colors.lightBlue,
+                                width: 1.5,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.lightBlue, width: 2.0),
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        (_isLoading == false)
+                            ? OutlinedButton(
+                                style: ButtonStyle(
+                                  overlayColor: MaterialStateProperty.all(
+                                      Colors.lightBlue),
+                                  foregroundColor: MaterialStateProperty.all(
+                                      Colors.lightBlue),
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(25)),
+                                  ),
+                                  side: MaterialStateProperty.all(
+                                      const BorderSide(
+                                          color: Colors.lightBlue)),
+                                ),
+                                onPressed: _isLoading ? null : _signIn,
+                                child: const Text('Obtener enlace de registro'),
+                              )
+                            : Platform.isAndroid
+                                ? const Center(
+                                    child: CircularProgressIndicator())
+                                : const Center(
+                                    child: CupertinoActivityIndicator())
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            (_isLoading == false)
-                ? ElevatedButton(
-                    onPressed: _isLoading ? null : _signIn,
-                    child: const Text('Obtener enlace de registro'),
-                  )
-                : Platform.isAndroid
-                    ? const Center(child: CircularProgressIndicator())
-                    : const Center(child: CupertinoActivityIndicator())
-          ],
+            ],
+          ),
         ),
       ),
     );
